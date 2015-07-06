800e76188d596b9af8604f12e033830fb77cf2af
Johnny Liu <johnnyliu@ideaculture.com>
2015-07-06 14:55:16 +0800

    Imported user code snippets

---
+++ objective-c_+initialize_method.m
@@ -0,0 +1,15 @@
+// Objective-C +initialize Method
+// Initialize a class before it receives its first message.
+//
+// IDECodeSnippetCompletionPrefix: initialize
+// IDECodeSnippetCompletionScopes: [ClassImplementation]
+// IDECodeSnippetIdentifier: C50BC316-B23E-400C-9E55-894F52F4AF69
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
++ (void)initialize
+{
+    if (self == [<#ClassName#> class]) {
+        <#statements#>
+    }
+}
